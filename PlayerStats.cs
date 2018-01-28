2bd4aac3fda7bd1af803cfcfba7ec18d5570c079
Ilham AJ <ilhamaryasuta@gmail.com>
2018-01-28 18:57:06 +0700

    Change the way to transfer player info

--- PlayerStats.cs
+++ PlayerStats.cs
@@ -7,35 +7,26 @@ namespace DiscordRP
     {
         public int updateTick = 0;
         public int maxUpdateTick = 180;
+        
         public override void OnEnterWorld(Player someone)
         {
             string wName = Main.worldName;
             bool expert = Main.expertMode;
             string wDiff = (expert) ? "(Expert)" : "(Normal)";
             RPControl.presence.details = string.Format("Playing {0} {1}", wName, wDiff);
+            updateTick = 180;
             RPControl.Update();
         }
-
+            
         public override void PostUpdate()
         {
             RPUtility.life = player.statLife;
             RPUtility.mana = player.statMana;
             RPUtility.def = player.statDefense;
 
-            if (player.ZoneCorrupt)
-                RPUtility.biome = RPUtility.Biomes.corrupt;
-            else if (player.ZoneCrimson)
-                RPUtility.biome = RPUtility.Biomes.crimson;
-            else if (player.ZoneJungle)
-                RPUtility.biome = RPUtility.Biomes.jungle;
-            else if (player.ZoneDesert)
-                RPUtility.biome = RPUtility.Biomes.desert;
-            else if (player.ZoneSnow)
-                RPUtility.biome = RPUtility.Biomes.snow;
-            else if (player.ZoneBeach)
-                RPUtility.biome = RPUtility.Biomes.ocean;
-            else
-                RPUtility.biome = RPUtility.Biomes.forest;
+            RPUtility.zone1 = player.zone1;
+            RPUtility.zone2 = player.zone2;
+            RPUtility.zone3 = player.zone3;
 
             if (updateTick < maxUpdateTick)
             {
--- RPUtility.cs
+++ RPUtility.cs
@@ -1,19 +1,13 @@
 ﻿using System.Timers;
+using Terraria;
 
 namespace DiscordRP
 {
     public static class RPUtility
     {
-        public enum Biomes
-        {
-            corrupt,
-            crimson,
-            jungle,
-            desert,
-            snow,
-            ocean,
-            forest
-        };
+        public static BitsByte zone1 = new BitsByte();
+        public static BitsByte zone2 = new BitsByte();
+        public static BitsByte zone3 = new BitsByte();
 
         public enum ATKType
         {
@@ -23,8 +17,6 @@ namespace DiscordRP
             thrown,
             summon
         };
-
-        public static Biomes biome = Biomes.forest;
 
         public static int life = 0;
         public static int mana = 0;
@@ -61,37 +53,135 @@ namespace DiscordRP
                     break;
             }
 
-            switch (biome)
+            if (zone1[3])
             {
-                case Biomes.corrupt:
-                    RPControl.presence.largeImageKey = string.Format("biome_corrupt");
-                    RPControl.presence.largeImageText = string.Format("Biome: Corruption");
-                    break;
-                case Biomes.crimson:
-                    RPControl.presence.largeImageKey = string.Format("biome_crimson");
-                    RPControl.presence.largeImageText = string.Format("Biome: Crimson");
-                    break;
-                case Biomes.jungle:
-                    RPControl.presence.largeImageKey = string.Format("biome_jungle");
-                    RPControl.presence.largeImageText = string.Format("Biome: Jungle");
-                    break;
-                case Biomes.desert:
-                    RPControl.presence.largeImageKey = string.Format("biome_desert");
-                    RPControl.presence.largeImageText = string.Format("Biome: Desert");
-                    break;
-                case Biomes.snow:
-                    RPControl.presence.largeImageKey = string.Format("biome_snow");
-                    RPControl.presence.largeImageText = string.Format("Biome: Snow");
-                    break;
-                case Biomes.ocean:
-                    RPControl.presence.largeImageKey = string.Format("biome_ocean");
-                    RPControl.presence.largeImageText = string.Format("Biome: Ocean");
-                    break;
-                case Biomes.forest:
-                    RPControl.presence.largeImageKey = string.Format("biome_forest");
-                    RPControl.presence.largeImageText = string.Format("Biome: Forest");
-                    break;
+                RPControl.presence.largeImageKey = string.Format("biome_meteor");
+                RPControl.presence.largeImageText = string.Format("Meteor");
             }
+            else if (zone3[4])
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_hell");
+                RPControl.presence.largeImageText = string.Format("Underworld");
+            }
+            else if (zone3[0])
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_sky");
+                RPControl.presence.largeImageText = string.Format("Space");
+            }
+            else if (zone1[6])
+            {
+                if (zone3[3])
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_ucrimson");
+                    RPControl.presence.largeImageText = string.Format("Underground Crimson");
+                }
+                else
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_crimson");
+                    RPControl.presence.largeImageText = string.Format("Crimson");
+                }
+            }
+            else if (zone1[1])
+            {
+                if (zone3[3])
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_ucorrupt");
+                    RPControl.presence.largeImageText = string.Format("Underground Corruption");
+                }
+                else
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_corrupt");
+                    RPControl.presence.largeImageText = string.Format("Corruption");
+                }
+            }
+            else if (zone1[2])
+            {
+                if (zone3[3])
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_uholy");
+                    RPControl.presence.largeImageText = string.Format("Underground Hollow");
+                }
+                else
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_holy");
+                    RPControl.presence.largeImageText = string.Format("Hollow");
+                }
+            }
+            else if (zone1[0])
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_dungeon");
+                RPControl.presence.largeImageText = string.Format("Dungeon");
+            }
+            else if (zone1[5])
+            {
+                if (zone3[3])
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_usnow");
+                    RPControl.presence.largeImageText = string.Format("Underground Snow");
+                }
+                else
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_snow");
+                    RPControl.presence.largeImageText = string.Format("Snow");
+                }
+            }
+            else if (zone2[7])
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_udesert");
+                RPControl.presence.largeImageText = string.Format("Underground Desert");
+            }
+            else if (zone2[5])
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_desert");
+                RPControl.presence.largeImageText = string.Format("Desert");
+            }
+            else if (zone1[4])
+            {
+                if (zone3[3]||zone3[2])
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_ujungle");
+                    RPControl.presence.largeImageText = string.Format("Underground Jungle");
+                }
+                else
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_jungle");
+                    RPControl.presence.largeImageText = string.Format("Jungle");
+                }
+            }
+            else if (zone2[6])
+            {
+                if (zone3[3] || zone3[2])
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_umushroom");
+                    RPControl.presence.largeImageText = string.Format("Underground Mushroom");
+                }
+                else
+                {
+                    RPControl.presence.largeImageKey = string.Format("biome_mushroom");
+                    RPControl.presence.largeImageText = string.Format("Mushroom");
+                }
+            }
+            else if (zone3[5])
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_ocean");
+                RPControl.presence.largeImageText = string.Format("Ocean");
+            }
+            else if (zone3[3])
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_cavern");
+                RPControl.presence.largeImageText = string.Format("Cavern");
+            }
+            else if (zone3[2])
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_underground");
+                RPControl.presence.largeImageText = string.Format("Underground");
+            }
+            else
+            {
+                RPControl.presence.largeImageKey = string.Format("biome_forest");
+                RPControl.presence.largeImageText = string.Format("Forest");
+            }
+
 
             RPControl.Update();
         }
